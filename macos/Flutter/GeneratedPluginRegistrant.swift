6d2b0423f9ac283563bd538d55b243913364423e
xochitlSalgado <seguraxochitl00@gmail.com>
2026-05-27 07:16:09 -0600

    Despliegue final: Galería de Arte lista para Render

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,13 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-=======
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
->>>>>>> 9ba01315fa07469fe69784858ebfc85f7a5d1467
 }
